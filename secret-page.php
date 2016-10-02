4d5fecce4a34c82e7678afe4a41ed6f149018d25
Sarah Heisselberg <sarah_heisselberg@hotmail.com>
2016-10-02 19:35:41 +0200

    Secret Page
    The super secret page can only be viewed by users who have created an
    account and signed in.

---
+++ secret-page.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>SUPER SECRET</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Rajdhani" rel="stylesheet" text="text/css">
+</head>
+
+<body>
+	<div class="header">
+		<a href="/php-login/index.php">LOG-IN SYSTEM - WELCOME TO THE SECRET PAGE</a>
+	</div>
+    
+    <img src="http://icons.iconarchive.com/icons/gordon-irving/leopard-extra-folders/512/Top-Secret-icon.png" alt="top secret" style="width:width;height:height;">
+
+</body>
+
+</html>
